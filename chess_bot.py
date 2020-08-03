6d4e3c2c713eba35b4e88e343b8a259d422e8392
emre <clkrv1@gmail.com>
2020-08-03 10:37:53 +0300

    works fine. refactored

--- chess_bot.py
+++ chess_bot.py
@@ -7,105 +7,125 @@ from stockfish import Stockfish
 from selenium.webdriver.common.action_chains import ActionChains
 from login_info import username, password
 
-def login():
-    email_in = driver.find_element_by_xpath('//*[@id="form3-username"]')
-    email_in.send_keys(username)
-    sleep(1)
-    pw_in = driver.find_element_by_xpath('//*[@id="form3-password"]')
-    pw_in.send_keys(password)
-    sleep(1)
-    driver.find_element_by_css_selector(".submit").click()
 
-def find_uci(ply, key):
-    uci_found = False
-    for pc in ply.split(":"):
-        if uci_found:
-            return pc.split(",")[0][1:-1]
-        if pc[-len(key):] == key:
-            uci_found = True
+LEFT_B_X = -300
+LEFT_B_Y = 300
+GAP = 80
+SLEEP_BETWEEN_MOVE_CLICKS = 0.01
+SLEEP_TIME_LOGGING_IN = 0.5
+SLEEP_TIME_MATCHING = 3
+TIME_FORMAT = "3+0"
+WHITE = "white"
+BLACK = "black"
 
-def find_ply(idx, script):
-    for i in range(idx + 1, len(script)):
-        if script[i] == "}":
-            return script[idx:i]
 
-def find_moves(key="{\"ply\""):
-    script = request_script()
-    moves = []
-    sans = []
-    for i in range(len(script) - len(key)):
-        if script[i:i+len(key)] == key:
-            moves.append(find_uci((tm:=find_ply(i, script)), key="\"uci\""))
-            sans.append(find_uci(tm, key="\"san\""))
-    return moves[1:], sans[1:]
+class LiChessBot():
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get('https://lichess.org')
+        self.engine = Stockfish(parameters={"Threads": 8})
+        self.engine.set_skill_level(20)
+        sleep(SLEEP_TIME_MATCHING)
+        self.last_color = WHITE
 
-def request_script():
-    page = requests.get2str(driver.current_url)
-    return BeautifulSoup(page, 'html.parser').find_all("script")[2].string
+    def enter_match(self, time_format):
+        self.driver.find_element_by_xpath(f'//*[@data-id="{TIME_FORMAT}"]/div').click()
+        sleep(SLEEP_TIME_MATCHING)
+        self.color = self.find_color()
 
-def click_to_coordinate(x, y):
-    ac = ActionChains(driver)
-    elem = driver.find_element_by_id("main-wrap")
-    ac.move_to_element(elem).move_by_offset(left_bottom_x + x * width, left_bottom_y - y * width).click().perform()
-
-def act(move, sleep_time):
-    # "e2e4"
-    #if move in {
-    #    "e8h8": "e8g8"
-    #}:
-    f, t = get_coordinates(move[:2]), get_coordinates(move[2:])
-    click_to_coordinate(*f)
-    sleep(sleep_time)
-    click_to_coordinate(*t)
+    def login(self):
+        self.driver.get('https://lichess.org/login')
+        sleep(SLEEP_TIME_LOGGING_IN)
+        self.driver.find_element_by_xpath('//*[@id="form3-username"]').send_keys(username)
+        sleep(SLEEP_TIME_LOGGING_IN)
+        self.driver.find_element_by_xpath('//*[@id="form3-password"]').send_keys(password)
+        sleep(SLEEP_TIME_LOGGING_IN)
+        self.driver.find_element_by_css_selector(".submit").click()
     
-def find_color():
-    # finding the color
-    for i in range(len((script := request_script())) - 6):
-        if script[i:i+6] == "player":
-            color = script[i+18:i+23]
-    return {"white": 0, "black": 1}[color]
+    def find_color(self):
+        # finding the color
+        for i in range(len((script := self.request_script())) - len((key:="\"player\":{\""))):
+            if script[i:i+len(key)] == key:
+                return {"white": WHITE, "black": BLACK}[script[i+19:i+24]]
 
-def get_coordinates(loc):
-    x, y = ord(loc[0])-97, int(loc[1])-1
-    if color:
-        return 7-x, 7-y
-    return x, y
+    def get_coordinates(self, loc):
+        x, y = ord(loc[0])-97, int(loc[1])-1
+        if self.color == BLACK:
+            return 7-x, 7-y
+        return x, y
 
-driver = webdriver.Firefox()
-driver.get('https://lichess.org')
-#driver.get('https://lichess.org/login')
-#sleep(1)
-#login()
+    def game_loop(self, time_format):
+        self.enter_match(time_format)
+        print(self.color)
+        while True:
+            moves, sans, colors = self.find_moves()
+            print(moves, sans, colors)
+            for i in range(1, len(moves)):
+                if sans[i] in ["O-O", "O-O-O"]:
+                    moves[i] = {
+                        "e8h8": "e8g8",
+                        "e1a1": "e1c1",
+                        "e8a8": "e8c8",
+                        "e1h1": "e1g1"
+                    }[moves[i]]
+            if self.color == colors[-1]:
+                self.move(moves)
+    
+    def move(self, moves):
+        self.engine.set_position(moves[1:])
+        self.act(self.engine.get_best_move())
+        
+    def click_to_coordinate(self, x, y):
+        ac = ActionChains(self.driver)
+        elem = self.driver.find_element_by_id("main-wrap")
+        ac.move_to_element(elem).move_by_offset(LEFT_B_X + x * GAP, LEFT_B_Y - y * GAP).click().perform()
 
-time_format = "3+0"
+    def act(self, move):
+        f, t = self.get_coordinates(move[:2]), self.get_coordinates(move[2:])
+        self.click_to_coordinate(*f)
+        sleep(SLEEP_BETWEEN_MOVE_CLICKS)
+        self.click_to_coordinate(*t)
 
-sleep(2)
-btn = driver.find_element_by_xpath(f'//*[@data-id="{time_format}"]/div')
-btn.click()
-sleep(2)
+    def request_script(self):
+        sleep(0.1)
+        page = requests.get2str(self.driver.current_url)
+        return BeautifulSoup(page, 'html.parser').find_all("script")[2].string
+            
+    def find_uci(self, ply):
+        ply = ply.split(":")
+        def find_key(key):
+            key_found = False
+            for pc in ply:
+                if key_found:
+                    return pc.split(",")[0][1:-1]
+                if pc[-len(key):] == key:
+                    key_found = True
+        uci = find_key("\"uci\"")
+        san = find_key("\"san\"")
+        return uci, san
 
-# orgX: -300, orgY: 300
-left_bottom_x = -300
-left_bottom_y = 300
-width = 80
+    def find_ply(self, idx, script):
+        for i in range(idx + 1, len(script)):
+            if script[i] == "}":
+                return script[idx:i]
 
-stockfish = Stockfish(parameters={"Threads": 8})
-stockfish.set_skill_level(20)
+    def find_moves(self):
+        script = self.request_script()
+        moves, sans, colors = [], [], []
+        key = "{\"ply\""
+        for i in range(len(script) - len(key)):
+            if script[i:i+len(key)] == key:
+                ply = self.find_ply(i, script)
+                uci, san = self.find_uci(ply)
+                color = WHITE if len(moves) % 2 == 0 else BLACK
+                moves.append(uci)
+                sans.append(san)
+                colors.append(color)
+        return moves, sans, colors
 
-color = find_color()
 
-while True:
-    moves, sans = find_moves()
-    if moves:
-        for i in range(len(moves)):
-            if sans[i] in ["O-O", "O-O-O"]:
-                moves[i] = {
-                    "e8h8": "e8g8",
-                    "e1a1": "e1c1",
-                    "e8a8": "e8c8",
-                    "e1h1": "e1g1"
-                }[moves[i]]
-    stockfish.set_position(moves)
-    move = stockfish.get_best_move()
-    act(move, 0.01)
-    print(moves, move)
+
+if __name__ == "__main__":
+    bot = LiChessBot()
+    #bot.login()
+    bot.game_loop(TIME_FORMAT)
